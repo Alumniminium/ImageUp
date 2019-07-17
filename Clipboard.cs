5737d685a215f8d7f3d206d166e84e8304b90df1
Yuki <domi@outlook.at>
2019-07-17 13:01:20 +0200

    Windows Support

--- Clipboard.cs
+++ Clipboard.cs
@@ -1,11 +1,67 @@
+using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace ImgUp
 {
     public static class Clipboard
     {
         public static void Set(string text)
+        {
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                SetLinux(text);
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                SetWindows(text);
+            else
+            Console.WriteLine("IDK HOW TO SET CLIPBOARD ON " +OSPlatform.OSX);
+        }
+
+        private static void SetWindows(string text)
+        {
+            try
+            {
+                var bytes = ((uint)text.Length + 1) * 2;
+                var hGlobal = GlobalAlloc(GMEM_MOVABLE, (UIntPtr)bytes);
+                
+                try
+                {
+                    var source = Marshal.StringToHGlobalUni(text);
+
+                    try
+                    {
+                        var target = GlobalLock(hGlobal);
+                        
+                        try
+                        {
+                            CopyMemory(target, source, bytes);
+                        }
+                        finally
+                        {
+                            GlobalUnlock(target);
+                        }
+
+                        hGlobal = IntPtr.Zero;
+                    }
+                    finally
+                    {
+                        Marshal.FreeHGlobal(source);
+                    }
+                }
+                finally
+                {
+                    if (hGlobal != IntPtr.Zero)
+                        GlobalFree(hGlobal);
+                }
+            }
+            finally
+            {
+                CloseClipboard();
+            }
+        }
+
+        private static void SetLinux(string text)
         {
             var tmpFilePath = Path.GetTempFileName();
             File.WriteAllText(tmpFilePath, text);
@@ -29,5 +85,26 @@ namespace ImgUp
                 File.Delete(tmpFilePath);
             }
         }
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GlobalAlloc(uint uFlags, UIntPtr dwBytes);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GlobalFree(IntPtr hMem);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GlobalLock(IntPtr hMem);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GlobalUnlock(IntPtr hMem);
+
+        [DllImport("kernel32.dll")]
+        public static extern void CopyMemory(IntPtr dest, IntPtr src, uint count);
+        
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseClipboard();
+
+        private const uint GMEM_MOVABLE = 0x0002;
     }
 }

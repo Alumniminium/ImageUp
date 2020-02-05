6eccacc16fd2e33e217d02bfcb48b3d919466c53
Trrbl <trrbl@her.st>
2020-02-05 10:37:53 +0100

    more logging, new endpoint

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,32 +10,62 @@ namespace ImgUp
 {
     public static class Program
     {
+        public static bool resize, convert;
+        public static int sizeh;
         public static AbstractClipboard Clipboard;
+        public static StreamWriter Logger = new StreamWriter("/home/alumni/imgup.log", true);
         public static async Task Main(string[] args)
         {
             if (args.Length == 0) // no args? no bueno.
                 return;// seppuku
+            Logger.WriteLine("Starting ImgUp...");
+            resize = args.Contains("-r");
+            Logger.WriteLine("Resize: "+resize);
+            convert = args.Contains("-c");
+            Logger.WriteLine("Convert: "+convert);
+            if (resize)
+                sizeh = int.Parse(args.First(a => a.Contains("--height=")).Split('=')[1]);
             
+            Logger.WriteLine("Size H: "+sizeh);
             var builder = new StringBuilder();
             for (var i = 0; i < args.Length; i++)
             {
-                // upload image, get direct link bacl
-                var url = await Uploader.UploadAsync(args[i]);
+                if (args[i].StartsWith("-"))
+                    continue;
+                
+            Logger.WriteLine("uploading "+args[i]);
+                var url = await Uploader.UploadAsync(WebPConverter.Convert(args[i]));
+
+                if (i == args.Length - 1 && !resize && !convert)
+                    builder.Append("image: " + url); // add to url list
+                else
+                    builder.AppendLine("image: " + url);
+
+                if (resize)
+                    args[i] = Processor.Resize(args[i], sizeh);
+
+                if (convert)
+                    args[i] = WebPConverter.Convert(args[i]);
+
+                // upload image, get direct link back
+                if (resize || convert)
+                    url = await Uploader.UploadAsync(args[i]);
                 // if this is the last file, don't add a new line at the end.
                 if (i == args.Length - 1)
-                    builder.Append(url); // add to url list
+                    builder.Append("thumbnail: " + url); // add to url list
                 else
-                    builder.AppendLine(url); // add line to url list
-
-                Console.WriteLine(url); // optional
+                    builder.AppendLine("thumbnail: " + url);
             }
-            if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                Clipboard= new LinuxClipboard();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                Clipboard = new LinuxClipboard();
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                Clipboard=new WindowsClipboard();
+                Clipboard = new WindowsClipboard();
 
             // set clipboard to the url list 
             Clipboard.Set(builder.ToString());
+            Console.WriteLine(builder.ToString());
+            Logger.WriteLine(builder.ToString());
+            Logger.Dispose();
         }// another kind of seppuku
     }
 }
